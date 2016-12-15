30ff2f178fab567b3e093e9caa61394f329256d9
Ikechi Michael <mykehell123@gmail.com>
2016-12-15 14:38:06 +0100

    added text colors

--- program.ps1
+++ program.ps1
@@ -1,8 +1,10 @@
 #"Hello, Michael";
 Write-Host "deploying on" $env:computername; #since we're all not Michael
 $myArray = "pinging host 127.0.0.1", "waiting for signal from localhost", "receiving bytes", "transpiling data", "checking bytes for mistransfers", "building HTTP packets", "encrypting data using SHA1";
+$myColors = "red", "green", "blue", "yellow";
 while ($true) {
     $myText = Get-Random -input $myArray -Count 1;
-    "`t... " + $myText;
+    $myColor = Get-Random -input $myColors -Count 1;
+    write-host ("`t... " + $myText) -foreground $myColor;
     Start-Sleep -Milliseconds 300;
 }
