4a6408cd7a50883b1630bac77e23867b070f7bc1
Ikechi Michael <mykehell123@gmail.com>
2016-12-15 09:51:39 +0100

    created program
    .bat and .ps1 files

---
+++ program.bat
@@ -0,0 +1,2 @@
+powershell -noexit -executionpolicy bypass -File "program.ps1"
+pause
---
+++ program.ps1
@@ -0,0 +1,7 @@
+"Hello, Michael";
+$myArray = "pinging host 127.0.0.1", "waiting for signal from localhost", "receiving bytes", "transpiling data", "checking bytes for mistransfers", "building HTTP packets", "encrypting data using SHA1";
+while ($true) {
+    $myText = Get-Random -input $myArray -Count 1;
+    "`t... " + $myText;
+    Start-Sleep -Milliseconds 300;
+}
